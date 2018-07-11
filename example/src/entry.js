59be86945db7488dbdc0c777c6768e73d6059cbe
叶展旺 <>
2018-07-11 20:39:13 +0800

    fixed

--- example/src/entry.js
+++ example/src/entry.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import zarmVue from 'zarm-vue'
 import App from './app.vue'
 
-import 'zarm-vue/zarm-vue.default.css'
-Vue.use(zarmVue)
 new Vue({
   el: '#app',
   render: h => h(App)
